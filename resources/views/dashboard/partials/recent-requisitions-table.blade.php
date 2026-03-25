e44ba3c967a5f8f9ece80a7c61eb6aea6183e70e
Shivendra <shivendrashukla.vspl@gmail.com>
2026-03-25 17:04:49 +0530

    aging modified

--- resources/views/dashboard/partials/recent-requisitions-table.blade.php
+++ resources/views/dashboard/partials/recent-requisitions-table.blade.php
@@ -239,7 +239,7 @@
 						@elseif($req->status === 'Pending Approval')
 						<span class="badge bg-info fs-9">Awaiting Approval</span>
 						@endif
-						@if($empStatus !== 'Inactive')
+						@if($candidate && $empStatus !== 'Inactive')
 
 						<a href="{{ route('submitter.agreement.view', $req) }}"
 							class="btn btn-sm btn-outline-{{ $req->status == 'Agreement Completed' ? 'success' : 'primary' }}"
