030fb789530029e4c8c3b4d42ac370dfcd93f1c9
Shivendra <shivendrashukla.vspl@gmail.com>
2026-04-04 15:06:46 +0530

    modified

--- app/Models/ManpowerRequisition.php
+++ app/Models/ManpowerRequisition.php
@@ -126,16 +126,14 @@ class ManpowerRequisition extends Model
         return $this->belongsTo(\App\Models\CoreVertical::class, 'vertical_id');
     }
 
-public function currentApprover()
-{
-    return $this->belongsTo(User::class, 'approver_id', 'emp_id')
-        ->whereExists(function ($query) {
-            $query->select(DB::raw(1))
-                ->from('core_employee')
-                ->whereColumn('core_employee.employee_id', 'users.emp_id')
-                ->where('core_employee.company_id', 1);
-        });
-}
+        public function currentApprover()
+        {
+            return $this->belongsTo(
+                \App\Models\Employee::class,
+                'approver_id',
+                'employee_id'
+            )->where('company_id', 1);
+        }
     // public function employeeGeneral()
     // {
     //     return $this->belongsTo(\App\Models\HrmEmployeeGeneral::class, 'submitted_by_employee_id', 'EmployeeID');
